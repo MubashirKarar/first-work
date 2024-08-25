34b280efb952557466f21dd4ae37d0ed71022ebc
mubashirkarar <mubashikarar73@gmail.com>
2024-08-25 17:12:24 +0500

    message

---
+++ main.dart
@@ -0,0 +1,19 @@
+void main (){
+  var name = "codewithowais";
+  print(name);
+  int num1 = 1000;
+  double num2 = 20;
+  num  num3 = 10.34;
+  num num4 = 54;
+ // print(num1);
+ // print(num2);
+ // print(num3);
+ // print(num4);
+ // print(num1 >num2);
+  print(false && true);
+  print(true&&true);
+  print(!true||!false);
+  String email ='codewithowais@gmail.com'
+  STRING password  ="3345678 "
+  string  userName = 'mubashir'
+  prnt (email==codewithowais@gmail.com ||userName=='mubashir')          
